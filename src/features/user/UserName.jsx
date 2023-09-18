0ca698b124060bc46de3e3534a4aa6c152fb13bb
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2023-09-18 17:04:23 +0700

    Connecting to Redux and Creating Global Store

--- src/features/user/UserName.jsx
+++ src/features/user/UserName.jsx
@@ -1,5 +1,10 @@
+import { useSelector } from "react-redux";
+
 function UserName() {
-  return <div className="text-sm font-semibold hidden md:block">Jonas</div>
+  const username = useSelector((state) => state.user.username);
+  return (
+    <div className="text-sm font-semibold hidden md:block">{username}</div>
+  );
 }
 
-export default UserName
+export default UserName;
